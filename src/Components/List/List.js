aa13b2e6d557c565462669faf9e7ca95572769ba
gitignore <m.barc@cyberfolks.pl>
2022-07-04 21:52:36 +0200

    state was added

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -16,15 +16,38 @@ const data = {
     surname: "Johanson",
     age: 35,
   },
+  3: {
+    name: "Albert",
+    surname: "Johanson",
+    age: 15,
+  },
+  4: {
+    name: "Kamil",
+    surname: "Johanson",
+    age: 85,
+  },
+  5: {
+    name: "Marcin",
+    surname: "Johanson",
+    age: 65,
+  },
+  6: {
+    name: "Dawid",
+    surname: "Johanson",
+    age: 25,
+  },
 };
 
 const myData = Object.values(data);
 const myDataToShow = myData.map((item, id) => {
   return { id, ...item };
 });
-console.log(myDataToShow);
+
+const myState = { ...myDataToShow };
+const state = Object.values(myState);
+
 export const List = () => {
-  return myDataToShow.map(({ id, name, surname, age }) => (
+  return state.map(({ id, name, surname, age }) => (
     <li key={id}>
       {age >= 18 ? <Item name={name} surname={surname} age={age} /> : null}
     </li>
