307f339fe258aa053944dac7e741d3f0b3f10ca4
gitignore <m.barc@cyberfolks.pl>
2022-07-05 11:03:36 +0200

    first version on 5.07

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import "./App.css";
 import List from "./Components/List/index";
+import React, { useState } from "react";
+
+const data = {
+  1: {
+    name: "john",
+    surname: "Smith",
+    age: 16,
+  },
+  2: {
+    name: "Will",
+    surname: "Johanson",
+    age: 35,
+  },
+  3: {
+    name: "Albert",
+    surname: "Johanson",
+    age: 15,
+  },
+  4: {
+    name: "Kamil",
+    surname: "Johanson",
+    age: 85,
+  },
+  5: {
+    name: "Marcin",
+    surname: "Johanson",
+    age: 65,
+  },
+  6: {
+    name: "Dawid",
+    surname: "Johanson",
+    age: 25,
+  },
+};
+
+const myData = Object.values(data);
+const myDataToShow = myData.map((item, id) => {
+  return { id, ...item, isActive: false };
+});
+
+const myState = { ...myDataToShow };
+const state = Object.values(myState);
+
+const toggleTasks = (taskId) => {};
+
+const deleteTask = (taskId) => {
+  console.log(taskId);
+  state.filter((item) => console.log(item.id));
+  const arrayFiltered = state.filter((item) => item.id !== taskId);
+  console.log(arrayFiltered);
+  return arrayFiltered;
+};
 
 const App = () => {
   return (
     <div className="App">
-      <List />
+      <List state={state} toggleTasks={toggleTasks} deleteTask={deleteTask} />
     </div>
   );
 };
--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,63 +1,11 @@
 import Item from "../Item/";
-import fetchMyData from "../FetchData/";
+import React from "react";
 
-// with async
-//const data = fetchMyData("https://squarit.cfolks.pl/dane.json");
-//const { results } = data;
+export const List = (props) => {
+  const { state } = props;
+  const { id, name, surname, age, isActive, toggleTask, deleteTask } = state;
 
-const data = {
-  1: {
-    name: "john",
-    surname: "Smith",
-    age: 16,
-  },
-  2: {
-    name: "Will",
-    surname: "Johanson",
-    age: 35,
-  },
-  3: {
-    name: "Albert",
-    surname: "Johanson",
-    age: 15,
-  },
-  4: {
-    name: "Kamil",
-    surname: "Johanson",
-    age: 85,
-  },
-  5: {
-    name: "Marcin",
-    surname: "Johanson",
-    age: 65,
-  },
-  6: {
-    name: "Dawid",
-    surname: "Johanson",
-    age: 25,
-  },
-};
-
-const myData = Object.values(data);
-const myDataToShow = myData.map((item, id) => {
-  return { id, ...item, isActive: false };
-});
-
-const myState = { ...myDataToShow };
-const state = Object.values(myState);
-
-export const List = () => {
-  const toggleTasks = () => {};
-
-  const deleteTasks = (taskId) => {
-    console.log(taskId.id);
-    const filteredDate = state.filter(
-      (id) => state.map(({ id }) => id) !== taskId.id
-    );
-    return filteredDate;
-  };
-
-  return state.map(({ id, name, surname, age, isActive }) => (
+  return state.map(() => (
     <li key={id}>
       {age >= 18 ? (
         <Item
@@ -66,8 +14,8 @@ export const List = () => {
           surname={surname}
           age={age}
           isActive={isActive}
-          toggleTasks={toggleTasks}
-          deleteTasks={deleteTasks}
+          toggleTasks={() => toggleTask(id)}
+          deleteTasks={() => deleteTask(id)}
         />
       ) : null}
     </li>
